ad92b848ace3b6fbf5b99c955df6a2439ae86a2c
Anthony <tony.ueta@gmail.com>
2023-05-10 22:27:40 -0300

    Continuando a página index

--- ProjetoFilme/ProjetoFilme/index.php
+++
@@ -1,5 +0,0 @@
-<?php include("cabecalho.php")?>
-
-    <div>
-        <h1>Destaque</h1> 
-    </div>
---
+++ ProjetoFilme/index.php
@@ -0,0 +1,24 @@
+<?php include("cabecalho.php")?>
+
+    <div>
+        <h1 class="titulo">Destaque</h1> 
+    </div>
+
+    <div>
+        <img src="img/opred.jpg" alt="Capa One Piece Film: RED" id="capaDestaque">
+
+        <div id="infoDestaque">
+            <p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+            <p>Goro Taniguchi</p><br>
+            <p><strong>RESUMO</strong></p><br>
+            <p>Em One Piece Film RED todos conhecerão Uta, a cantora mais amada do planeta, cuja voz foi descrita 
+            como “de outro mundo”. Ela é conhecida por esconder sua própria identidade ao se apresentar. Agora, 
+            pela primeira vez, Uta se revelará ao mundo em um show ao vivo. Com a Marinha assistindo de perto, o local 
+            se enche de fãs de Uta - incluindo piratas animados e os Chapéus de Palha liderados por Luffy, que vieram 
+            para curtir sua performance - todos aguardam ansiosamente a voz que o mundo inteiro estava esperando.</p><br>
+        </div>
+    </div>
+
+    <div class="generos">
+        <h1 class="titulo">Fantasia</h1>
+    </div>
