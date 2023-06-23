6706bfe224c73f4045006309efbd7e60975eddcc
fabioantunes2806 <fabioantunes2806@gmail.com>
2023-06-23 07:23:47 -0300

    Arrumando a index

--- estruturas/catalogo.php
+++ estruturas/catalogo.php
@@ -1,103 +1,137 @@
 <?php include("conexao.php"); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    
-<title>Index</title>
-
-    
-<div class="background">
-        <?php
+<?php
             $stmt = $pdo->prepare("select * from filmes where Nome_filme = 'One Piece Film: Red'");	
             $stmt ->execute();
 
             ($row = $stmt ->fetch(PDO::FETCH_BOTH)); ?>
 
-            <h1 class="titulo">Destaque</h1>
-            <div class="container"> 
-                <div class="container__generosIndex"> 
-                    <img id="imgDestaque" src="<?php echo $row[8] ?>" alt="Capa One Piece Film: RED"> 
-                    <div id="container_destaque">          
-                        <p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
-                        <p class="container_destaque__info"><?php echo "<td> $row[5] </td>" ?></p><br>
-                        <p><strong>RESUMO</strong></p><br>
-                        <p class="container_destaque__info"><?php echo "<td> $row[6] </td>" ?></p><br>
-                        <p><strong>Gênero</strong></p><br>
-                        <p class="container_destaque__info"><?php 
-                                $stmt = $pdo->prepare("select * from genero where id_genero=4");	
-                                    $stmt ->execute();            
-                                    ($row = $stmt ->fetch(PDO::FETCH_BOTH));
-                                    echo $row[1];  ?></p> 
-                                    </div>
-                    </div>
-                </div> 
+<h1 class="titulo">Destaque</h1>
+		<div class="container"> 
+			<img class="container-img" src="<?php echo $row[8] ?>" alt="Capa One Piece Film: RED">
+			<div id="container-destaque">          
+				<p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[5] ?>
+				</p><br>
+				<p><strong>RESUMO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[6] ?>
+				</p><br>
+				<p><strong>Gênero</strong></p><br>
+				<p class="container-destaque-info">
+                <?php  $stmt = $pdo->prepare("select * from genero where id_genero= $row[4]");	
+                        $stmt ->execute();            
+                        ($row = $stmt ->fetch(PDO::FETCH_BOTH));
+                        echo $row[1];  ?>
+				</p> 
+			</div>
+		</div> 
 
-            <h1 class="titulo">Fantasia</h1>
-            <?php
-                        $stmt = $pdo->prepare("SELECT * FROM `filmes` WHERE id_filme = 3");	
-                        $stmt ->execute();
-                        
-                        ($row = $stmt ->fetch(PDO::FETCH_BOTH)) ?>
-            <section class="container">    
-                <div class="container__generosIndex">
-                <img class="filme" src="<?php echo $row[8] ?>" alt="Cartaz Yu-Gi-Oh" class="proporcaoFilme">
-                <div id="container_destaque">
-                <p class=container_destaque__info><?php echo $row[1]; ?> </p><br>
-                <p> <?php echo $row[6]; ?> <p>
-                    </div>
-                </div>
-            </section>
+     <?php   $stmt = $pdo->prepare("select * from filmes where id_filme = 3");	
+            $stmt ->execute();
 
-            <h1 class="titulo">Terror</h1>
-            <?php
-                        $stmt = $pdo->prepare("SELECT * FROM `filmes` WHERE id_filme = 5");	
-                        $stmt ->execute();
-                        
-                        ($row = $stmt ->fetch(PDO::FETCH_BOTH)) ?>
-            <section class="container">    
-                <div class="container__generosIndex">
-                <img class="filme" src="<?php echo $row[8] ?>" alt="Cartaz Invocação do Mal" class="proporcaoFilme">
-                <div id="container_destaque">
-                <p class=container_destaque__info><?php echo $row[1]; ?> </p><br>
-                <p> <?php echo $row[6]; ?> <p>
-                    </div>
-                </div>
-            </section>
+            ($row = $stmt ->fetch(PDO::FETCH_BOTH)); ?>
 
-            <h1 class="titulo">Ação e Aventura</h1>
-            <?php
-                        $stmt = $pdo->prepare("SELECT * FROM `filmes` WHERE id_filme = 8");	
-                        $stmt ->execute();
-                        
-                        ($row = $stmt ->fetch(PDO::FETCH_BOTH)) ?>
-            <section class="container">    
-                <div class="container__generosIndex">
-                <img class="filme" src="<?php echo $row[8] ?>" alt="Cartaz Yu-Gi-Oh" class="proporcaoFilme">
-                <div id="container_destaque">
-                <p class=container_destaque__info><?php echo $row[1]; ?> </p><br>
-                <p> <?php echo $row[6]; ?> <p>
-                    </div>
-                </div>
-            </section>
+		<h1 class="titulo">Fantasia</h1>
+		<div class="container"> 
+			<img class="container-img" src="<?php echo $row[8] ?>" alt="Capa One Piece Film: RED">
+			<div id="container-destaque">          
+				<p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+				<p class="container-destaque-info">
+                  <?php echo $row[5] ?>
+				</p><br>
+				<p><strong>RESUMO</strong></p><br>
+				<p class="container-destaque-info">
+                     <?php echo $row[6] ?>
+				</p><br>
+				<p><strong>Gênero</strong></p><br>
+				<p class="container-destaque-info">
+                <?php  $stmt = $pdo->prepare("select * from genero where id_genero= $row[4]");	
+                        $stmt ->execute();            
+                        ($row = $stmt ->fetch(PDO::FETCH_BOTH));
+                        echo $row[1];  ?>
+				</p> 
+			</div>
+		</div>
 
+        <?php   $stmt = $pdo->prepare("select * from filmes where id_filme = 5");	
+            $stmt ->execute();
 
-            <?php
-                        $stmt = $pdo->prepare("SELECT * FROM `contato` WHERE id_contato = 2");	
-                        $stmt ->execute();
-                        
-                        ($row = $stmt ->fetch(PDO::FETCH_BOTH)) ?>
-            <div>
-                <?php echo "Usuário: $row[1]"; ?> <br>
-                <h6><?php echo "Assunto:$row[3]"; ?><br>
-                <?php echo "Mensagem: $row[4]";?>
-            </div> 
-    </div>
+            ($row = $stmt ->fetch(PDO::FETCH_BOTH)); ?>
 
-</body>
-</html>
+		<h1 class="titulo">Terror</h1>
+		<div class="container"> 
+			<img class="container-img" src=" <?php echo $row[8] ?>" alt="Capa One Piece Film: RED">
+			<div id="container-destaque">          
+				<p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+				<p class="container-destaque-info">
+                  <?php echo $row[5] ?>
+				</p><br>
+				<p><strong>RESUMO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[6] ?>
+				</p><br>
+				<p><strong>Gênero</strong></p><br>
+				<p class="container-destaque-info">
+					 <?php  $stmt = $pdo->prepare("select * from genero where id_genero= $row[4]");	
+                        $stmt ->execute();            
+                        ($row = $stmt ->fetch(PDO::FETCH_BOTH));
+                        echo $row[1];  ?>
+				</p> 
+			</div>
+		</div>
 
+        <?php   $stmt = $pdo->prepare("select * from filmes where id_filme = 8");	
+            $stmt ->execute();
+
+            ($row = $stmt ->fetch(PDO::FETCH_BOTH)); ?>
+
+		<h1 class="titulo">Ação e Aventura</h1>
+		<div class="container"> 
+			<img class="container-img" src="<?php echo $row[8] ?>" alt="Capa One Piece Film: RED">
+			<div id="container-destaque">          
+				<p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+				<p class="container-destaque-info">
+                 <?php echo $row[5] ?>
+				</p><br>
+				<p><strong>RESUMO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[6] ?>
+				</p><br>
+				<p><strong>Gênero</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php  $stmt = $pdo->prepare("select * from genero where id_genero= $row[4]");	
+                        $stmt ->execute();            
+                        ($row = $stmt ->fetch(PDO::FETCH_BOTH));
+                        echo $row[1];  ?>
+				</p> 
+			</div>
+		</div>
+
+        <?php   $stmt = $pdo->prepare("select * from filmes where id_filme = 35");	
+            $stmt ->execute();
+
+            ($row = $stmt ->fetch(PDO::FETCH_BOTH)); ?>
+
+		<h1 class="titulo">Comédia</h1>
+		<div class="container"> 
+			<img class="container-img" src="  <?php echo $row[8] ?>" alt="Capa One Piece Film: RED">
+			<div id="container-destaque">          
+				<p><strong>DIRETOR DE ANIMAÇÃO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[5] ?>
+				</p><br>
+				<p><strong>RESUMO</strong></p><br>
+				<p class="container-destaque-info">
+                    <?php echo $row[6] ?>
+				</p><br>
+				<p><strong>Gênero</strong></p><br>
+				<p class="container-destaque-info">
+                <?php  $stmt = $pdo->prepare("select * from genero where id_genero= $row[4]");	
+                        $stmt ->execute();            
+                        ($row = $stmt ->fetch(PDO::FETCH_BOTH));
+                        echo $row[1];  ?>
+				</p> 
+			</div>
+		</div>
